77ee70851f6701786fbeb942987b9010b5fbddac
Jim Turner <turnerjw784@yahoo.com>
2017-12-22 19:51:17 -0600

    Fix header comment documenting how to build.

--- contrib/FauxdaciousUrlHelper.pl
+++ contrib/FauxdaciousUrlHelper.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 #MUST INSTALL youtube-dl FOR Youtube to work!
-#pp -o FauxdaciousUrlHelper.exe -M Tunein::Streams -M IHeartRadio::Streams -M WWW::YouTube::Download -M utf8_heavy.pl -l libeay32_.dll -l zlib1_.dll -l ssleay32_.dll FauxdaciousUrlHelper.pl
+#pp -o FauxdaciousUrlHelper.exe -M StreamFinder::Facebook -M StreamFinder::IHeartRadio -M StreamFinder::Radionomy -M StreamFinder::Reciva -M StreamFinder::Youtube -M WWW::YouTube::Download -M utf8_heavy.pl -l libeay32_.dll -l zlib1_.dll -l ssleay32_.dll FauxdaciousUrlHelper.pl
 
 #FAUXDACIOUS "HELPER" SCRIPT TO HANDLE URLS THAT FAUXDACIOUS CAN'T PLAY DIRECTLY:
 
