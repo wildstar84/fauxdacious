7cdd5f4980f0b71641aa0b214d4764ce8ecfd8bf
Jim Turner <turnerjw784@yahoo.com>
2019-11-18 14:41:59 -0600

    contrib - note a cpl module updates & add helper build source.

---
+++ contrib/urlhelper_mods.pm
@@ -0,0 +1,20 @@
+#!/perl/bin/perl
+
+use URI::Escape;
+use HTML::Entities;
+use LWP::UserAgent;
+use StreamFinder;
+use StreamFinder::Apple;
+use StreamFinder::BannedVideo;
+use StreamFinder::Brighteon;
+use StreamFinder::IHeartRadio;
+use StreamFinder::RadioNet;
+use StreamFinder::Radionomy;
+use StreamFinder::Reciva;
+use StreamFinder::Tunein;
+use StreamFinder::Vimeo;
+use StreamFinder::Youtube;
+
+################# END PAR COMPILER SECTION ##################
+
+1;
