018c1250233b05354da1a7fd2119e664af8b3ff7
Jim Turner <turnerjw784@yahoo.com>
2019-11-17 23:03:42 -0600

    images - Workaround Win32 svg bug - see: Audacious commits# 6de6e65 (2019/5/01) & c4f19a5 (2019/08/13). about.cc: Revert back to png image for Fauxdacious logo (svg fails on Windows due to same bug - that I don't know how to actual fix in the image file).  TODO:  Equalizer-preset button icons (folder & record).svg also need this regex fix, so don't show on button in Windows for now.

--- src/libfauxdgui/about.cc
+++ src/libfauxdgui/about.cc
@@ -78,8 +78,8 @@ static GtkWidget * create_about_window ()
     GtkWidget * vbox = gtk_vbox_new (false, 6);
     gtk_container_add ((GtkContainer *) about_window, vbox);
 
-    AudguiPixbuf logo (gdk_pixbuf_new_from_resource ("/org/fauxdacious/about-logo.svg", nullptr));
-    GtkWidget * image = gtk_image_new_from_pixbuf (logo.get ());
+    StringBuf logo_path = filename_build ({data_dir, "images", "about-logo.png"});
+    GtkWidget * image = gtk_image_new_from_file (logo_path);
     gtk_box_pack_start ((GtkBox *) vbox, image, false, false, 0);
 
     GtkWidget * label = gtk_label_new (nullptr);
