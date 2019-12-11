2ceec33a42730c4011c1ac2cc3fe97b048491ed9
Jim Turner <turnerjw784@yahoo.com>
2019-12-11 09:28:32 -0600

    Aud: Various font-entry chgs. (11/9) See: Audacious commits# 3309976, 74e26d8, 4e49b74, 2cc6c70, 36d1301, c4eb81b, 21f2075, and 469bb69.

--- src/libfauxdqt/font-entry.cc
+++ src/libfauxdqt/font-entry.cc
@@ -34,7 +34,7 @@ class FontEntry : public QLineEdit {
 public:
     FontEntry (QWidget * parent = nullptr, const char * font = nullptr) :
         QLineEdit (parent),
-        m_action (get_icon ("dialog-text-and-font"), _("Set Font"), nullptr)
+        m_action (get_icon ("preferences-desktop-font"), _("Set Font"), nullptr)
     {
         addAction (& m_action, TrailingPosition);
         connect (& m_action, & QAction::triggered, this, & FontEntry::show_dialog);
@@ -46,7 +46,6 @@ public:
     }
 
 private:
-    QFontDialog * create_dialog ();
     void show_dialog ();
 
     QAction m_action;
@@ -54,7 +53,7 @@ private:
 };
 
 /* parse a subset of Pango font descriptions */
-static QFont * qfont_from_string (const char * name)
+EXPORT QFont qfont_from_string (const char * name)
 {
     auto family = str_copy (name);
     int size = 0;
@@ -95,101 +94,71 @@ static QFont * qfont_from_string (const char * name)
 
         if (! attr_found)
         {
-            auto font = new QFont ((const char *) family);
+            QFont font ((const char *) family);
 
-            /* check for a recognized font family */
-            if (! space || font->exactMatch ())
-            {
-                if (size > 0)
-                    font->setPointSize (size);
-                if (weight != QFont::Normal)
-                    font->setWeight (weight);
-                if (style != QFont::StyleNormal)
-                    font->setStyle (style);
-                if (stretch != QFont::Unstretched)
-                    font->setStretch (stretch);
+            if (size > 0)
+                font.setPointSize (size);
+            if (weight != QFont::Normal)
+                font.setWeight (weight);
+            if (style != QFont::StyleNormal)
+                font.setStyle (style);
+            if (stretch != QFont::Unstretched)
+                font.setStretch (stretch);
 
-                return font;
-            }
-
-            delete font;
+            return font;
         }
 
         family.resize (space - family);
     }
 }
 
-QFontDialog * FontEntry::create_dialog ()
+EXPORT StringBuf qfont_to_string (const QFont & font)
 {
-    auto dialog = new QFontDialog (this);
+    StringBuf font_str = str_copy (font.family ().toUtf8 ());
 
-    QObject::connect (dialog, & QFontDialog::fontSelected, [this] (const QFont & font) {
-        auto family = font.family ().toUtf8 ();
+    auto weight = font.weight ();
+    auto style = font.style ();
+    auto stretch = font.stretch ();
 
-        // build the description string
-        StringBuf font_str = str_copy ((const char *) family);
+    if (weight == QFont::Light)
+        font_str.insert (-1, " Light");
+    else if (weight == QFont::Bold)
+        font_str.insert (-1, " Bold");
 
-        auto weight = font.weight ();
-        auto style = font.style ();
-        auto stretch = font.stretch ();
+    if (style == QFont::StyleOblique)
+        font_str.insert (-1, " Oblique");
+    else if (style == QFont::StyleItalic)
+        font_str.insert (-1, " Italic");
 
-        if (weight == QFont::Light)
-            font_str = str_concat({font_str, " Light"});
-        else if (weight == QFont::Bold)
-            font_str = str_concat({font_str, " Bold"});
+    if (stretch == QFont::Condensed)
+        font_str.insert (-1, " Condensed");
+    else if (stretch == QFont::Expanded)
+        font_str.insert (-1, " Expanded");
 
-        if (style == QFont::StyleOblique)
-            font_str = str_concat({font_str, " Oblique"});
-        else if (style == QFont::StyleItalic)
-            font_str = str_concat({font_str, " Italic"});
+    str_append_printf (font_str, " %d", font.pointSize ());
 
-        if (stretch == QFont::Condensed)
-            font_str = str_concat ({font_str, " Condensed"});
-        else if (stretch == QFont::Expanded)
-            font_str = str_concat ({font_str, " Expanded"});
-
-        font_str = str_concat({font_str, " ", int_to_str (font.pointSize ())});
-
-        font_entry_set_font (this, font_str);
-    });
-
-    return dialog;
+    return font_str;
 }
 
 void FontEntry::show_dialog ()
 {
     if (! m_dialog)
-        m_dialog = create_dialog ();
-
-    auto font = qfont_from_string (font_entry_get_font (this));
-    if (! font)
     {
-        window_bring_to_front (m_dialog);
-        return;
+        m_dialog = new QFontDialog (this);
+
+        QObject::connect (m_dialog, & QFontDialog::fontSelected, [this] (const QFont & font) {
+            setText ((const char *) qfont_to_string (font));
+            end (false);
+        });
     }
 
-    m_dialog->setCurrentFont (*font);
-    delete font;
-
+    m_dialog->setCurrentFont (qfont_from_string (text ().toUtf8 ()));
     window_bring_to_front (m_dialog);
 }
 
 EXPORT QLineEdit * font_entry_new (QWidget * parent, const char * font)
 {
     return new FontEntry (parent, font);
-}
-
-EXPORT String font_entry_get_font (QLineEdit * entry)
-{
-    QByteArray text = entry->text ().toUtf8 ();
-
-    return String (text);
-}
-
-EXPORT void font_entry_set_font (QLineEdit * entry, const char * font)
-{
-    entry->setText (font);
-    entry->end (false);
 }
 
 } // namespace audqt
