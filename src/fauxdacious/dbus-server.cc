6723d99e528228d568d84b9b6be4352e5b3de87f
Jim Turner <turnerjw784@yahoo.com>
2020-12-11 17:55:48 -0600

    aud - Update copyright per Audacious commit#b19868b.

--- src/fauxdacious/dbus-server.cc
+++ src/fauxdacious/dbus-server.cc
@@ -1,6 +1,6 @@
 /*
  * dbus-server.c
- * Copyright 2013 John Lindgren
+ * Copyright 2013-2020 John Lindgren
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
