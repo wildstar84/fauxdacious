e45b80300796f2210003c15a9fa2c940f3b039ca
Jim Turner <turnerjw784@yahoo.com>
2024-05-29 22:06:26 -0500

    aud - faudqt: Dark theme adjustments: - Darken base color slightly from #383838 to #303030 - Fix very low contrast of toggle buttons (e.g. shuffle/repeat) - Make widget QProxyStyles inherit from DarkStyle (Audacious commit# 7741ee9)

--- src/libfauxdqt/dark-theme.cc
+++ src/libfauxdqt/dark-theme.cc
@@ -28,6 +28,7 @@
 #include <QProxyStyle>
 #include <QStyle>
 #include <QStyleFactory>
+#include <QStyleOption>
 
 namespace audqt {
 
@@ -88,9 +89,26 @@ class DarkStyle : public QProxyStyle
 public:
     DarkStyle() : QProxyStyle("fusion") {}
 
-    void polish(QApplication * app) override { QProxyStyle::polish(app); };
+    void polish(QApplication * app) override { QProxyStyle::polish(app); }
     void polish(QWidget * widget) override { QProxyStyle::polish(widget); }
     void polish(QPalette & palette) override;
+
+    void drawPrimitive(PrimitiveElement elem, const QStyleOption * option,
+                       QPainter * painter,
+                       const QWidget * widget) const override;
+
+    /* Qt 6.3+ no longer uses the theme icon "window-close" for tabs,
+     * but instead forces a built-in icon. Override it back. */
+#if QT_VERSION >= QT_VERSION_CHECK(6, 3, 0)
+    QIcon standardIcon(StandardPixmap standardIcon,
+                       const QStyleOption * option = nullptr,
+                       const QWidget * widget = nullptr) const override
+    {
+        if (standardIcon == QStyle::SP_TabCloseButton)
+            return QIcon::fromTheme("window-close");
+        return QProxyStyle::standardIcon(standardIcon, option, widget);
+    }
+#endif
 };
 
 void DarkStyle::polish(QPalette & palette)
@@ -98,6 +116,8 @@ void DarkStyle::polish(QPalette & palette)
     // normal colors
     // clang-format off
     palette.setColor(QPalette::WindowText,      QColor(0xf0, 0xf0, 0xf0));
+    // note that QFusionStyle brightens the Button color significantly:
+    // #2c2c2c actually results in a gradient from #3f3f3f to #363636
     palette.setColor(QPalette::Button,          QColor(0x2c, 0x2c, 0x2c));
     palette.setColor(QPalette::Light,           QColor(0x70, 0x70, 0x70));
     palette.setColor(QPalette::Midlight,        QColor(0x60, 0x60, 0x60));
@@ -107,7 +127,7 @@ void DarkStyle::polish(QPalette & palette)
     palette.setColor(QPalette::BrightText,      QColor(0xf0, 0xf0, 0xf0));
     palette.setColor(QPalette::ButtonText,      QColor(0xf0, 0xf0, 0xf0));
     palette.setColor(QPalette::Base,            QColor(0x20, 0x20, 0x20));
-    palette.setColor(QPalette::Window,          QColor(0x38, 0x38, 0x38));
+    palette.setColor(QPalette::Window,          QColor(0x30, 0x30, 0x30));
     palette.setColor(QPalette::Shadow,          QColor(0x00, 0x00, 0x00));
     palette.setColor(QPalette::Highlight,       QColor(0x15, 0x53, 0x9e)); // matches Adwaita-dark
     palette.setColor(QPalette::HighlightedText, QColor(0xff, 0xff, 0xff));
@@ -131,6 +151,32 @@ void DarkStyle::polish(QPalette & palette)
     palette.setColor(QPalette::Disabled, QPalette::LinkVisited,   QColor(0x80, 0x80, 0x80));
     palette.setColor(QPalette::Disabled, QPalette::AlternateBase, QColor(0x30, 0x30, 0x30));
     // clang-format on
+}
+
+void DarkStyle::drawPrimitive(PrimitiveElement elem,
+                              const QStyleOption * option, QPainter * painter,
+                              const QWidget * widget) const
+{
+    if (elem == PE_PanelButtonCommand &&
+        ((option->state & (State_Sunken | State_On))))
+    {
+        // QFusionStyle draws pressed buttons with very low contrast,
+        // which makes it difficult to see if shuffle/repeat are active.
+        // As a workaround, darken them via palette adjustment.
+        QStyleOption option_copy = *option;
+        option_copy.palette.setColor(QPalette::Button,
+                                     QColor(0x1c, 0x1c, 0x1c));
+        QProxyStyle::drawPrimitive(elem, &option_copy, painter, widget);
+    }
+    else
+    {
+        QProxyStyle::drawPrimitive(elem, option, painter, widget);
+    }
+}
+
+QStyle *create_dark_style()
+{
+    return new DarkStyle();
 }
 
 void enable_dark_theme()
